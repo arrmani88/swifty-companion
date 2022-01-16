ed175f455290b4d95bf533269a7abc9b910e4497
Anas El boudali <anel-bou@student.1337.ma>
2022-01-16 22:29:02 +0100

    update

--- lib/routes/home_route.dart
+++ lib/routes/home_route.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rounded_loading_button/rounded_loading_button.dart';
 
 class HomeRoute extends StatelessWidget {
   const HomeRoute({Key? key}) : super(key: key);
@@ -9,10 +10,28 @@ class HomeRoute extends StatelessWidget {
       body: SafeArea(
         child: Container(
           child: Center(
-            child: Column(
-              children: [
-                Image.asset('assets/images/app_logo.jpeg', height: 150.0,)
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset('assets/images/app_logo.jpeg', height: 250.0,),
+                  const SizedBox(height: 30.0,),
+                  Row(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Theme.of(context).secondaryHeaderColor, // fillColor: Colors.white,
+                          label: Text('Enter login here'),
+                          border: OutlineInputBorder()
+                        ),
+                      ),
+                      RoundedLoadingButton(controller: controller, onPressed: onPressed, child: child)
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
