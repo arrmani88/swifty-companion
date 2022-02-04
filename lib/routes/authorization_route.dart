623c801a80e9bf8c800825e2805a6bbd95c2f6c4
Anas El boudali <anel-bou@student.1337.ma>
2022-02-04 16:57:13 +0100

    update

--- lib/routes/authorization_route.dart
+++ lib/routes/authorization_route.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:swifty_companion/widgets/loading_wrapper.dart';
 
 class AuthorizationRoute extends StatelessWidget {
   const AuthorizationRoute({Key? key}) : super(key: key);
@@ -6,26 +8,41 @@ class AuthorizationRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: RadialGradient(colors: [Theme.of(context).splashColor, Theme.of(context).scaffoldBackgroundColor], radius: 0.8)
-        ),
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Click on th lock to allow Swifty Companion use your 42intra account',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white, fontSize: 25.0),
-                ),
-                InkWell(child: Image.asset('assets/icons/lock_icon.png', height: 250.0,)),
-              ],
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                gradient: RadialGradient(colors: [Theme.of(context).splashColor, Theme.of(context).scaffoldBackgroundColor], radius: 0.8)
             ),
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Click on the lock to allow Swifty Companion use your 42intra account',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    ),
+                    const SizedBox(height: 40.0),
+                    InkWell(
+                      child: Image.asset('assets/icons/lock_icon.png', height: 250.0),
+                      onTap: () {},
+                    ),
+                    const SizedBox(height: 40.0),
+                    const Text(
+                      'You\'ll be redirected to the 42 intra website to authorize the use of your account',
+                      style: TextStyle(color: Color(0xffa3a3a3), fontSize: 16.5),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
+            )
           ),
-        )
+          const LoadingWrapper(),
+        ],
       ),
     );
   }
---
+++ lib/widgets/loading_wrapper.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'dart:ui';
+
+class LoadingWrapper extends StatelessWidget {
+  const LoadingWrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+          child: Container(
+            color: Colors.black.withOpacity(0.6),
+            height: 200.0,
+            width: 500.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Loading ...', style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                const SizedBox(height: 20.0),
+                LoadingAnimationWidget.dotsTriangle(color: Colors.white, size: 100.0),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+
+        Container(
+          color: Colors.black.withOpacity(0.7),
+          height: 200.0,
+          width: 500.0,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Loading ...', style: TextStyle(color: Colors.white, fontSize: 25.0)),
+              const SizedBox(height: 20.0),
+              LoadingAnimationWidget.dotsTriangle(color: Colors.white, size: 100.0),
+            ],
+          ),
+        )
+
+*/
